32ae0aaaacd79f853637d51951d4b8d850f50beb
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2022-11-26 19:04:40 -0300

    Retirando p5 bibliotecas,adicionando sons e campo de futebol

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
+// Criando componente Canvas para dsenhos em 2D
 let canvas = document.querySelector("#idCanvas");
 var pincel = canvas.getContext("2d");
-
 
 //ValorES da bolinha x e y
 let bolinhaX = 250;
@@ -20,33 +20,55 @@ let yRaquete = 200;
 let xRaquete2 = 475;
 let yRaquete2 = 200;
 
-let taxa = 25;
+let taxa = 25; //nao sei o que faz
 
-// códigos do teclado
+// códigos dos teclado para interação
 var cima = 38;
 var baixo = 40;
-
 var w = 87;
 var s = 83;
 
-
+// Sons
+let rebater = new Audio('sons/rebater.wav');
+let gol = new Audio('sons/gol.mp3');
 // função que desenha o grid.
 function limpaTela() {
   var descer = 0;
   while (descer <= 500) {
     for (var imp = 0; imp <= 500; imp = imp + 25) {
       pincel.fillStyle = "black";
-      pincel.strokeStyle = "blue";
+      //pincel.strokeStyle = "blue";
       pincel.beginPath();
       pincel.rect(imp, descer, 25, 25);
       pincel.closePath();
       pincel.fill();
-      pincel.stroke();
+    //  pincel.stroke();
     }
     descer = descer + 25;
   }
 }
+function desenharCampo() {
+  //Meio do campo
+  pincel.fillStyle = 'green';
+  pincel.beginPath();
+  pincel.rect(245, 0, 10, 500);
+  pincel.fill();
 
+  //Meio campo bola
+  pincel.fillStyle = 'green';
+  pincel.beginPath();
+  pincel.arc(250, 250, 40, 0, 2 * Math.PI);
+  pincel.fill();
+
+  //Meio campo bola2
+  pincel.fillStyle = 'white';
+  pincel.beginPath();
+  pincel.arc(250, 250, 10, 0, 2 * Math.PI);
+  pincel.fill();
+
+
+  
+}
 
 // função que cria a bolinha
 function desenhaCirculo(raio) {
@@ -110,10 +132,11 @@ function verificaColisaoRaquete1() {
   if (bolinhaX == 35) { //bolinha na raquete 1
     diferenca = bolinhaY - yRaquete;
     if (bolinhaY >= yRaquete && diferenca >= 0 && diferenca <= 150) {
+      rebater.play();
       verificarAngulo1();
     }
   }
-  
+
 }
 
 
@@ -123,11 +146,13 @@ function verificaColisaoRaquete2() {
     console.log("bolinha: " + bolinhaY);
     diferenca = bolinhaY - yRaquete2;
     if (bolinhaY >= yRaquete2 && diferenca >= 0 && diferenca <= 150) {
+      rebater.play();
       verificarAngulo2();
     }
   }
-  
+
 }
+
 //se pegar mais pra cima da raquete,bola vai para cima (primeiros 75 pixels)
 //se pegar mais pra cima da raquete,bola vai para cima (dos 76 pixels até o 150 px)
 function verificarAngulo1() {
@@ -149,14 +174,24 @@ function verificarAngulo2() {
     bolinhaParaCima = true;
   }
 }
+
+function verificarGol() {
+  console.log(bolinhaX)
+  if(bolinhaX == 0 || bolinhaX == 495) {
+    gol.play();
+  }
+}
+
 function atualizaTela() {
+  
   limpaTela();
+  desenharCampo();
   desenhaCirculo(10);
   desenhaRaquete1(larguraRaquete1, alturaRaquete1, xRaquete, yRaquete);
   desenhaRaquete2(larguraRaquete1, alturaRaquete1, xRaquete2, yRaquete2);
   verificaColisaoRaquete1();
   verificaColisaoRaquete2();
-  //verificar gol function verificarGol();
+   verificarGol();
 }
 
 function leDoTeclado(evento) {
